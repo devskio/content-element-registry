927100db31841a25e2de6f70daa8cb036a73bbde
Ondrej Grosko <ondrej.grosko@devsk.io>
2023-08-23 12:57:18 +0200

    [TASK] - Alias Class loader

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'Digitalwerk\\ContentElementRegistry\\ContentElement\\AbstractContentElementRegistryItem' => \Devsk\ContentElementRegistry\ContentElement\AbstractContentElementRegistryItem::class,
+];
