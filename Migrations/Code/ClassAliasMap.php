7a3e2918ea3f3e2434c08ec073c2f03370068eb1
Ondrej Grosko <ondrej.grosko@devsk.io>
2023-08-23 13:13:50 +0200

    [TASK] - Alias Class loader & LegacyClassesForIde

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -1,4 +1,14 @@
 <?php
 return [
     'Digitalwerk\\ContentElementRegistry\\ContentElement\\AbstractContentElementRegistryItem' => \Devsk\ContentElementRegistry\ContentElement\AbstractContentElementRegistryItem::class,
+    'Digitalwerk\\ContentElementRegistry\\Core\\ContentElementRegistry' => \Devsk\ContentElementRegistry\Core\ContentElementRegistry::class,
+    'Digitalwerk\\ContentElementRegistry\\Core\\ContentElementRegistryException' => \Devsk\ContentElementRegistry\Core\ContentElementRegistryException::class,
+    'Digitalwerk\\ContentElementRegistry\\DataProcessing\\ContentElementObjectDataProcessor' => \Devsk\ContentElementRegistry\DataProcessing\ContentElementObjectDataProcessor::class,
+    'Digitalwerk\\ContentElementRegistry\\DataProcessing\\HeadlessDataProcessor' => \Devsk\ContentElementRegistry\DataProcessing\HeadlessDataProcessor::class,
+    'Digitalwerk\\ContentElementRegistry\\Domain\\Model\\ContentElement' => \Devsk\ContentElementRegistry\Domain\Model\ContentElement::class,
+    'Digitalwerk\\ContentElementRegistry\\Domain\\Model\\Relation' => \Devsk\ContentElementRegistry\Domain\Model\Relation::class,
+    'Digitalwerk\\ContentElementRegistry\\Events\\ContentElementRegistryClassEvent' => \Devsk\ContentElementRegistry\Events\ContentElementRegistryClassEvent::class,
+    'Digitalwerk\\ContentElementRegistry\\Hook\\ContentElementPreviewRenderer' => \Devsk\ContentElementRegistry\Hook\ContentElementPreviewRenderer::class,
+    'Digitalwerk\\ContentElementRegistry\\Traits\\Injection\\InjectDataMapper' => \Devsk\ContentElementRegistry\Traits\Injection\InjectDataMapper::class,
+    'Digitalwerk\\ContentElementRegistry\\Utility\\ContentElementRegistryUtility' => \Devsk\ContentElementRegistry\Utility\ContentElementRegistryUtility::class,
 ];
---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,94 @@
+<?php
+namespace {
+    die('Access denied');
+}
+
+namespace Digitalwerk\ContentElementRegistry\ContentElement {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class AbstractContentElementRegistryItem extends \Devsk\ContentElementRegistry\ContentElement\AbstractContentElementRegistryItem
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Core {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElementRegistry extends \Devsk\ContentElementRegistry\Core\ContentElementRegistry
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Core {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElementRegistryException extends \Devsk\ContentElementRegistry\Core\ContentElementRegistryException
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\DataProcessing {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElementObjectDataProcessor extends \Devsk\ContentElementRegistry\DataProcessing\ContentElementObjectDataProcessor
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\DataProcessing {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class HeadlessDataProcessor extends \Devsk\ContentElementRegistry\DataProcessing\HeadlessDataProcessor
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Domain\Model {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElement extends \Devsk\ContentElementRegistry\Domain\Model\ContentElement
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Domain\Model {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class Relation extends \Devsk\ContentElementRegistry\Domain\Model\Relation
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Events {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElementRegistryClassEvent extends \Devsk\ContentElementRegistry\Events\ContentElementRegistryClassEvent
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Hook {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElementPreviewRenderer extends \Devsk\ContentElementRegistry\Hook\ContentElementPreviewRenderer
+    {}
+}
+
+namespace Digitalwerk\ContentElementRegistry\Traits\Injection {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    trait InjectDataMapper
+    {
+        use \Devsk\ContentElementRegistry\Traits\Injection\InjectDataMapper;
+    }
+}
+
+namespace Digitalwerk\ContentElementRegistry\Utility {
+    /**
+     * @deprecated since 11.6.0, removed since 12.0
+     */
+    class ContentElementRegistryUtility extends \Devsk\ContentElementRegistry\Utility\ContentElementRegistryUtility
+    {}
+}
